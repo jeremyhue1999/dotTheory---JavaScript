09bea1182c2c011b72b2aa66e97114b9e8a388a6
Jeremy Datud <jeremy.hue.datud@codeandtheory.com>
2022-09-02 08:19:51 +0800

    creates the tasks for DOM interaction training

---
+++ JS-203: Interacting with the DOM /challenge/challenge2.js
@@ -0,0 +1,8 @@
+//Adds a condition that changes the background of the <input> elements only.
+let section = document.getElementsByTagName("section")[0];
+
+section.addEventListener("click", (e) => {
+  if (e.target.tagName === "INPUT") {
+    e.target.style.backgroundColor = "rgb(255, 255, 0)";
+  }
+});
