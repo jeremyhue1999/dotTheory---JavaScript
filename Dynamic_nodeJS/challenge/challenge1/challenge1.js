03dd2da83f3a114c5ec8945cc022e346faf6478d
Jeremy <jeremy.hue.datud@codeantheory.com>
2022-10-07 14:48:39 +0800

    JS-401: Builds Dynamic site

---
+++ Dynamic_nodeJS/challenge/challenge1/challenge1.js
@@ -0,0 +1,25 @@
+// creates an about function that prints out the response and exports the function
+
+function root(request, response) {
+    if(request.url == "/") {
+        response.writeHead(200, {'Content-type': "text/plain"});
+        response.end("Home\n");
+    }
+}
+
+function contact(request, response) {
+    if(request.url == "/contact") {
+        response.writeHead(200, {'Content-type': "text/plain"});
+        response.end("Contact\n");
+    }
+}
+function about(request, response) {
+  if(request.url === "/about"){
+    response.writeHead(200, {'Content-type': "text/plain"});
+        response.end("About\n");
+}
+}
+
+module.exports.root = root;
+module.exports.contact = contact;
+module.exports.about = about; 
