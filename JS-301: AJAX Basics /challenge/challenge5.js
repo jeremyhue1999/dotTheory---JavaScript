5a2d8bbbe9d9a1439a7ac4b0a418451f1aa1b5b9
Jeremy <jeremy.hue.datud@codeantheory.com>
2022-10-28 11:07:40 +0800

    JS-301: Refactored Necessary codes

--- JS-301: AJAX Basics /challenge/challenge5.js
+++ JS-301: AJAX Basics /challenge/challenge5.js
@@ -4,11 +4,9 @@
 //uses a DOM element's innerHTML property to set the HTML inside the sidebar div to the server's response.
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-    }
-    document.getElementById("sidebar").innerHTML = xhr.responseText;
+  if (xhr.readyState === 4 && xhr.status === 200) {
   }
+  document.getElementById("sidebar").innerHTML = xhr.responseText;
 };
 xhr.open("GET", "sidebar.html");
 xhr.send();
